0e7ccf42c4bf5b17a6725bfb5648af68ded5a6e7
smellstink <54760319+OceanNut420@users.noreply.github.com>
2023-01-21 12:51:41 -0800

    code4life

--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -12,20 +12,41 @@ public class Calculator {
 		int number2 = Integer.parseInt(input2);
 		int task = JOptionPane.showOptionDialog(null, "What do you choose?", "Calculator", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
-
+		double number = 0;
 		// 3) Call the correct method depending on what option the user chooses
-		
+		if(task == 3) {
+			number = add(number1, number2);
+		} if(task == 2) {
+			number = subtract(number1, number2);
+		} if(task == 1) {
+			number = multiply(number1, number2);
+		} if(task == 0) {
+			number = divide(number1, number2);
+		}
 		// 4) Call the result() method and put the answer in a pop-up
+		JOptionPane.showMessageDialog(null, "Your number is number " + number);
 	}
-	
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
-	 
-	
+	public static int add(int number1, int number2) {	
+		return number1 + number2;	
+	}
+	public static int subtract(int number1, int number2) {
+		return number1 - number2;
+	}
+	public static int multiply(int number1, int number2) {
+		return number1 * number2;
+	}
+	public static double divide(int number1, int number2) {
+		double d1 = number1;
+		double d2 = number2;
+		return d1/d2;
+	}
+
 	// 2) Make a static method called " result" that takes a number
 	//		as a parameter and returns the number in a string
 	//		EX: "Your answer is " + number;
 
-	
+
 }
--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -8,6 +8,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class Code4Life {
+		
 	/***********  SOUND ***************
 	 * Some computers are unable to play sounds. 
 	 * If you cannot play sound on this computer, set canPlaySounds to false.
@@ -15,21 +16,32 @@ public class Code4Life {
 	 * *****************/
 	static boolean canPlaySounds = true;
 	
+	public static void main(String[] args) {
+		
 	
 	// 1. Ask the user how many hours they spent coding this week.
-
+	String hours = JOptionPane.showInputDialog("How many hours this week have you spent coding");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+	int hoursAsInt = Integer.parseInt(hours);
+	if(hoursAsInt<=2) {
+		JOptionPane.showMessageDialog(null, "Stop watching youtube and go code instead!");
+	} if(hoursAsInt>=3 && hoursAsInt<=5) {
+		JOptionPane.showMessageDialog(null, "You're a code ninja!!");
+	} if(hoursAsInt>5) {
+		playBatmanTheme();
+		displayBatman();
+	}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
 
 	// 4. If it is more than 5, call the playBatmanTheme  method, then 
 	//    call the displayBatman method.
-
-
-
 	
+	
+
+
+	}
 	private static void playBatmanTheme() {
 	// NOTE: Download batman.wav from league-sounds on GitHub: https://github.com/jointheleague/league-sounds
 	//	     Then put batman.wav into the "_2_code_4_life" project
